3436435ba12d8e45791a7128068c678b7435ed12
Behruz-developer <88540036+Behruz-developer@users.noreply.github.com>
2024-03-23 14:42:35 +0500

    finish

---
+++ script.js
@@ -0,0 +1,56 @@
+// let n1 = +prompt("son kriting");
+// while (isNaN(n1) || n1 == 0) {
+//     n1 = +prompt('bu yerga son kritish kerak')
+// }
+
+// for (let i = 0; i < n1; i++) {
+
+// }
+
+// function name1() {
+//     let operations = ['+', '-', '*', '/', '%'];
+//     let index = Math.floor(Math.random() * operations.length);
+//     let n1 = Math.ceil(Math.random()*10)
+//     let n2 = Math.ceil(Math.random()*10)
+
+//     let a = +prompt(`${n1} ${operations[index]}  ${n2}`)
+
+//     if(operations[index] == '+'){
+//         result = n1 + n2
+//         if (result == a) {
+//             console.log('sizning javob togri' + a);
+//         }else{
+//             console.log('sizning javob notogri' + a + 'togri javob'+ result);
+//         }
+//     }else if (operations[index] == '-'){
+//         result = n1 + n2
+//         if (result == a) {
+//             console.log('sizning javob togri ' + a);
+//         }else{
+//             console.log('sizning javob notogri ' + a + ' togri javob '+ result);
+//         }
+//     }else if (operations[index] == '*'){
+//         result = n1 * n2
+//         if (result == a) {
+//             console.log('sizning javob togri ' + a);
+//         }else{
+//             console.log('sizning javob notogri ' + a + ' togri javob '+ result);
+//         }
+//     }else if (operations[index] == '/'){
+//         result = n1 / n2
+//         if (result == a) {
+//             console.log('sizning javob togri ' + a);
+//         }else{
+//             console.log('sizning javob notogri ' + a + ' togri javob '+ result);
+//         }
+//     }else if (operations[index] == '%'){
+//         result = n1 % n2
+//         if (result == a) {
+//             console.log('sizning javob togri ' + a);
+//         }else{
+//             console.log('sizning javob notogri ' + a + ' togri javob '+ result);
+//         }
+//     }
+
+// }
+// name1()
