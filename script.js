27d4d206ff1ac7ff96fc6e5a0fda720396079652
Behruz-developer <88540036+Behruz-developer@users.noreply.github.com>
2024-03-23 14:43:18 +0500

    j-6.1

--- script.js
+++ script.js
@@ -1,56 +1,56 @@
-// let n1 = +prompt("son kriting");
-// while (isNaN(n1) || n1 == 0) {
-//     n1 = +prompt('bu yerga son kritish kerak')
-// }
+let n1 = +prompt("son kriting");
+while (isNaN(n1) || n1 == 0) {
+    n1 = +prompt('bu yerga son kritish kerak')
+}
 
-// for (let i = 0; i < n1; i++) {
+for (let i = 0; i < n1; i++) {
 
-// }
+}
 
-// function name1() {
-//     let operations = ['+', '-', '*', '/', '%'];
-//     let index = Math.floor(Math.random() * operations.length);
-//     let n1 = Math.ceil(Math.random()*10)
-//     let n2 = Math.ceil(Math.random()*10)
+function name1() {
+    let operations = ['+', '-', '*', '/', '%'];
+    let index = Math.floor(Math.random() * operations.length);
+    let n1 = Math.ceil(Math.random()*10)
+    let n2 = Math.ceil(Math.random()*10)
 
-//     let a = +prompt(`${n1} ${operations[index]}  ${n2}`)
+    let a = +prompt(`${n1} ${operations[index]}  ${n2}`)
 
-//     if(operations[index] == '+'){
-//         result = n1 + n2
-//         if (result == a) {
-//             console.log('sizning javob togri' + a);
-//         }else{
-//             console.log('sizning javob notogri' + a + 'togri javob'+ result);
-//         }
-//     }else if (operations[index] == '-'){
-//         result = n1 + n2
-//         if (result == a) {
-//             console.log('sizning javob togri ' + a);
-//         }else{
-//             console.log('sizning javob notogri ' + a + ' togri javob '+ result);
-//         }
-//     }else if (operations[index] == '*'){
-//         result = n1 * n2
-//         if (result == a) {
-//             console.log('sizning javob togri ' + a);
-//         }else{
-//             console.log('sizning javob notogri ' + a + ' togri javob '+ result);
-//         }
-//     }else if (operations[index] == '/'){
-//         result = n1 / n2
-//         if (result == a) {
-//             console.log('sizning javob togri ' + a);
-//         }else{
-//             console.log('sizning javob notogri ' + a + ' togri javob '+ result);
-//         }
-//     }else if (operations[index] == '%'){
-//         result = n1 % n2
-//         if (result == a) {
-//             console.log('sizning javob togri ' + a);
-//         }else{
-//             console.log('sizning javob notogri ' + a + ' togri javob '+ result);
-//         }
-//     }
+    if(operations[index] == '+'){
+        result = n1 + n2
+        if (result == a) {
+            console.log('sizning javob togri' + a);
+        }else{
+            console.log('sizning javob notogri' + a + 'togri javob'+ result);
+        }
+    }else if (operations[index] == '-'){
+        result = n1 + n2
+        if (result == a) {
+            console.log('sizning javob togri ' + a);
+        }else{
+            console.log('sizning javob notogri ' + a + ' togri javob '+ result);
+        }
+    }else if (operations[index] == '*'){
+        result = n1 * n2
+        if (result == a) {
+            console.log('sizning javob togri ' + a);
+        }else{
+            console.log('sizning javob notogri ' + a + ' togri javob '+ result);
+        }
+    }else if (operations[index] == '/'){
+        result = n1 / n2
+        if (result == a) {
+            console.log('sizning javob togri ' + a);
+        }else{
+            console.log('sizning javob notogri ' + a + ' togri javob '+ result);
+        }
+    }else if (operations[index] == '%'){
+        result = n1 % n2
+        if (result == a) {
+            console.log('sizning javob togri ' + a);
+        }else{
+            console.log('sizning javob notogri ' + a + ' togri javob '+ result);
+        }
+    }
 
-// }
-// name1()
+}
+name1()
